0ffd05328d625728f000437d77782f10e210abce
Jb Galvo <jbmgalvo16@gmail.com>
2023-09-30 14:59:17 +0800

    added title and button section

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let dice = document.getElementById("dice");
+
+const min = 1;
+const max = 24;
+
+dice.onclick = function () {
+  let xRand = getRandom(max, min);
+  let yRand = getRandom(max, min);
+
+  dice.style.webkitTransform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+  dice.style.transform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+};
+
+function getRandom(max, min) {
+  return (Math.floor(Math.random() * (max - min)) + min) * 90;
+}
