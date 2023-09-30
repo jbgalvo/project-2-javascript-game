10cf4846329e9017c02162694391095444ec504d
Jb Galvo <jbmgalvo16@gmail.com>
2023-09-30 18:39:26 +0800

    updated user interface for the pig dice game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
-let dice = document.getElementById("dice");
+// let dice = document.getElementById("dice");
 
-const min = 1;
-const max = 24;
+// const min = 1;
+// const max = 24;
 
-dice.onclick = function () {
-  let xRand = getRandom(max, min);
-  let yRand = getRandom(max, min);
+// dice.onclick = function () {
+//   let xRand = getRandom(max, min);
+//   let yRand = getRandom(max, min);
 
-  dice.style.webkitTransform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
-  dice.style.transform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
-};
+//   dice.style.webkitTransform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+//   dice.style.transform = "rotateX(" + xRand + "deg) rotateY(" + yRand + "deg)";
+// };
 
-function getRandom(max, min) {
-  return (Math.floor(Math.random() * (max - min)) + min) * 90;
-}
+// function getRandom(max, min) {
+//   return (Math.floor(Math.random() * (max - min)) + min) * 90;
+// }
+
+
+//Decleration of Variables
+
